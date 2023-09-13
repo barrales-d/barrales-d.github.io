645b31030fe5fea566393fbaecc20155b6774e60
barrales-d <dbarrales06@gmail.com>
2023-09-13 14:18:21 -0700

    New model caption

--- scripts/animations.js
+++ scripts/animations.js
@@ -18,14 +18,12 @@ $(document).on("scroll", () => {
 //  DEMO CARASEL ANIMATION
 var current_demo = 0
 var demo_list = [
-    "ghosa-cute.gif",
-    "bloo_sprite_idle.gif"
+    "files/skill/ghosa-cute.gif",
+    "files/skill/bloo_sprite_idle.gif"
 ]
 function demoSwitch() {
-    var skill_demo = $("#Demo");
-    var demo = "files/skill/" + demo_list[current_demo];
-    
-    $(skill_demo).attr("src", demo);
+    var skill_demo = $("#Demo");    
+    $(skill_demo).attr("src", demo_list[current_demo]);
     current_demo = (current_demo + 1) % demo_list.length
 }
 
@@ -33,6 +31,12 @@ $(document).ready(()=> {
     setInterval(demoSwitch, 5000);
 });
 
+function cutExtension(src) {
+    return src.substring(0, src.indexOf("."));
+}
+function cutLeadingURL(src) {
+    return src.substring(src.lastIndexOf('/') + 1);
+}
 // MODAL ZOOM IN ANIMATION
 var modal = $("#Modal")
 var skill_demo = $("#Demo");
@@ -42,8 +46,9 @@ $(skill_demo).on("click", () => {
     var caption = $("#Caption");
 
     $(modal).css("display", "block");
-    $(modal_img).attr("src", $(skill_demo).attr("src"));
-    $(caption).text($(skill_demo).attr("alt"));
+    var src_text = $(skill_demo).attr("src");
+    $(modal_img).attr("src", src_text);
+    $(caption).text(cutLeadingURL(cutExtension(src_text)).toUpperCase());
 });
 
 $("._close").on("click", function() {
