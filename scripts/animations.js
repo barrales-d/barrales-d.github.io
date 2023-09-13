94b2045a28b0d9a14e2b9a602454b95ddc0d2be2
barrales-d <dbarrales06@gmail.com>
2023-09-13 13:58:06 -0700

    redesign portfolio

---
+++ scripts/animations.js
@@ -0,0 +1,51 @@
+// SLIDE IN ANIMATION
+$(document).on("scroll", () => {
+    var page_top = $(document).scrollTop();
+    var page_bottom = page_top + $(window).height();
+
+    var slide_in_objs = $("._slide_in");
+
+    for(var cur_obj = 0; cur_obj < slide_in_objs.length; cur_obj++) {
+        var obj = slide_in_objs[cur_obj];
+
+        if ($(obj).position().top < page_bottom) {
+            $(obj).addClass("_visible");
+          } else {
+            $(obj).removeClass("_visible");
+          }
+    }
+});
+//  DEMO CARASEL ANIMATION
+var current_demo = 0
+var demo_list = [
+    "ghosa-cute.gif",
+    "bloo_sprite_idle.gif"
+]
+function demoSwitch() {
+    var skill_demo = $("#Demo");
+    var demo = "files/" + demo_list[current_demo];
+    
+    $(skill_demo).attr("src", demo);
+    current_demo = (current_demo + 1) % demo_list.length
+}
+
+$(document).ready(()=> {
+    setInterval(demoSwitch, 5000);
+});
+
+// MODAL ZOOM IN ANIMATION
+var modal = $("#Modal")
+var skill_demo = $("#Demo");
+
+$(skill_demo).on("click", () => {
+    var modal_img = $("#ModalImage");
+    var caption = $("#Caption");
+
+    $(modal).css("display", "block");
+    $(modal_img).attr("src", $(skill_demo).attr("src"));
+    $(caption).text($(skill_demo).attr("alt"));
+});
+
+$("._close").on("click", function() {
+    $(modal).css("display","none");
+});
---
+++ scripts/projects.js
@@ -0,0 +1,16 @@
+$(document).ready(() => {
+
+    var projects = $("._projects");
+
+    $.getJSON("files/projects.json", (json_data) => {
+        $.each(json_data, (project_count, project_data) => {
+            var card = `<div class="_project_card">
+                                <img src=${(project_data.img) ? project_data.img : "files/bubbles.png" } alt="Project ${project_count + 1}" >
+                                <h3>${project_data.title}</h3>
+                                <p>${project_data.desc}</p>
+                                <a href=${project_data.link} target="_blank">View on GitHub</a>
+                        </div>`;
+            projects.append(card);
+        });
+    });
+});
